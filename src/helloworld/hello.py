ca71db28c25f3335dee9c54c348e2d8343ffc8db
kevinflint-cs2 <kevin.flint@cybersecuritysquared.com>
2025-07-17 13:55:00 -0700

    chore: initial commit of helloworld package

---
+++ src/helloworld/hello.py
@@ -0,0 +1,10 @@
+# src/helloworld/hello.py
+
+class HelloWorld:
+    """Simple example class."""
+    def greet(self) -> str:
+        return "Hello, world!"
+
+if __name__ == "__main__":
+    hw = HelloWorld()
+    print(hw.greet())
---
+++ tests/test_helloworld.py
@@ -0,0 +1,7 @@
+# tests/test_helloworld.py
+
+from helloworld.hello import HelloWorld
+
+def test_greet():
+    hw = HelloWorld()
+    assert hw.greet() == "Hello, world!"
