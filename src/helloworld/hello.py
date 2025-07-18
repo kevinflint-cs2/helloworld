7a6dcada0b20cb07cfe6c16cb3f4fe9e89998388
Flint, Kevin (HQP) <kevin.flint@roberthalf.com>
2025-07-18 12:57:02 -0700

    chore: add GitHub Codespaces devcontainer

--- src/helloworld/hello.py
+++ src/helloworld/hello.py
@@ -1,9 +1,12 @@
 # src/helloworld/hello.py
 
+
 class HelloWorld:
     """Simple example class."""
+
     def greet(self) -> str:
         return "Hello, world!"
+
 
 if __name__ == "__main__":
     hw = HelloWorld()
--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -2,6 +2,7 @@
 
 from helloworld.hello import HelloWorld
 
+
 def test_greet():
     hw = HelloWorld()
     assert hw.greet() == "Hello, world!"
